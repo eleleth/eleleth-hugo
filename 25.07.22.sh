866e08ec4cffd3e7a8e4e8a2ccacdf02e59d5a66
Shane Mulligan <mullikine@gmail.com>
2022-07-25 17:23:19 +1200

    25.07.22

--- 25.07.22.sh
+++ 25.07.22.sh
@@ -6,3 +6,7 @@ cd "/home/shane/source/git/eleleth/eleleth-hugo";  go mod download # "<==" hugo
 cd "/home/shane/source/git/eleleth/eleleth-hugo";  go list -m -json all # "<==" hugo
 cd "/home/shane/source/git/eleleth/eleleth-hugo";  go mod download "github.com/FortAwesome/Font-Awesome@v0.0.0-20210804190922-7d3d774145ac" # "<==" hugo
 cd "/home/shane/source/git/eleleth/eleleth-hugo";  go list -m -json "github.com/FortAwesome/Font-Awesome@v0.0.0-20210804190922-7d3d774145ac" # "<==" hugo
+cd "/home/shane/source/git/eleleth/eleleth-hugo";  go mod download # "<==" hugo
+cd "/home/shane/source/git/eleleth/eleleth-hugo";  go list -m -json all # "<==" hugo
+cd "/home/shane/source/git/eleleth/eleleth-hugo";  go mod download "github.com/FortAwesome/Font-Awesome@v0.0.0-20210804190922-7d3d774145ac" # "<==" hugo
+cd "/home/shane/source/git/eleleth/eleleth-hugo";  go list -m -json "github.com/FortAwesome/Font-Awesome@v0.0.0-20210804190922-7d3d774145ac" # "<==" hugo
